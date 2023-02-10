26b6d54a37105fc8786f37a1fd87e2170c435042
Tianying0902 <Aimee_wang0902@163.com>
2023-02-10 15:35:22 +0800

    [Tianying]feat: build the framework firstly

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TodoForm } from './components/TodoForm';
 import './App.css';
 
 function App() {
+  const [todos,setTodos] = useState<Array<Todo>>([]);
+const addTodo:AddTodo = newTodo=>{
+  if(newTodo!==" "){
+    setTodos([...todos,{text:newTodo,completed:false}]);
+  }
+}
   return (
     <div className="App">
       <h1 className='header'>todos</h1>
+      <TodoForm addTodo={addTodo}/>
     </div>
   );
 }
