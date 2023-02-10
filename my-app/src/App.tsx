12d4858bd7dd51ed1f9258d2e6ffc9908c8518be
Tianying0902 <Aimee_wang0902@163.com>
2023-02-10 17:52:11 +0800

    [Tianying]feat: add a new task to TodoList

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { TodoForm } from './components/TodoForm';
+import { TodoList} from './components/TodoList';
+// import { TodoItem } from './components/TodoItem';
 import './App.css';
 
 function App() {
@@ -9,10 +11,19 @@ const addTodo:AddTodo = newTodo=>{
     setTodos([...todos,{text:newTodo,completed:false}]);
   }
 }
+const toggleCompleted:ToggleCompleted = selectedTodo =>{
+ const updateTodo = todos.map((todo) =>{
+  if(todo===selectedTodo){
+    return {...todo,completed:!todo.completed}
+  }return todo;
+ });
+ setTodos(updateTodo)
+};
   return (
     <div className="App">
       <h1 className='header'>todos</h1>
       <TodoForm addTodo={addTodo}/>
+      <TodoList todoData={todos} toggleCompleted={toggleCompleted }/>
     </div>
   );
 }
